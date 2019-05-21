66d2d4e868cdcf6bb75b0c6d7e4c82bcf1476aab
Zdenek Valek <zdenek.valek@nxp.com>
2019-05-21 17:48:03 +0200

    Added new preEAR version 0.4.2 for Linux version 4.19

--- drivers/net/ethernet/stmicro/stmmac/dwmac-s32cc.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-s32cc.c
@@ -1,7 +1,5 @@
 /*
- * dwmac-s32cc.c - S32 GMAC glue layer
- *
- * Author: Jan Petrous <jan.petrous@nxp.com>
+ * dwmac-s32cc.c - S32x GMAC glue layer
  *
  * Copyright 2019 NXP
  *
@@ -12,7 +10,7 @@
  * published by the Free Software Foundation.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <linux/device.h>
@@ -26,21 +24,10 @@
 
 #include "stmmac_platform.h"
 
-struct s32_dwmac {
-	struct device	*dev;
-	void __iomem	*reg;
-};
-
-void s32_dwmac_fix_mac_speed(void *priv, unsigned int speed)
-{
-	/* no need for now (as on vdk) */
-}
-
 static int s32cc_dwmac_probe(struct platform_device *pdev)
 {
 	struct plat_stmmacenet_data *plat_dat;
 	struct stmmac_resources stmmac_res;
-	struct s32_dwmac *dwmac = NULL;
 	struct resource *res;
 	int ret;
 
@@ -61,25 +48,6 @@ static int s32cc_dwmac_probe(struct platform_device *pdev)
 	plat_dat->has_gmac4 = true;
 	plat_dat->pmt = 1;
 	plat_dat->tso_en = of_property_read_bool(pdev->dev.of_node, "snps,tso");
-
-#if 0
-	dwmac = devm_kzalloc(&pdev->dev, sizeof(*dwmac), GFP_KERNEL);
-	if (!dwmac) {
-		ret = -ENOMEM;
-		goto err_remove_config_dt;
-	}
-
-	dwmac->reg = stmmac_res->addr;
-	if (!(dwmac->reg)) {
-		ret = EINVAL;
-		goto err_remove_config_dt;
-	}
-
-	dwmac->dev = &pdev->dev;
-#endif
-
-	//TODO: plat_dat->fix_mac_speed = s32_dwmac_fix_mac_speed;
-	plat_dat->bsp_priv = dwmac;
 
 	ret = stmmac_dvr_probe(&pdev->dev, plat_dat, &stmmac_res);
 	if (ret)
@@ -111,6 +79,6 @@ static struct platform_driver s32_dwmac_driver = {
 module_platform_driver(s32_dwmac_driver);
 
 MODULE_AUTHOR("Jan Petrous <jan.petrous@nxp.com>");
-MODULE_DESCRIPTION("NXP S32 common chassis DWMAC glue layer");
+MODULE_DESCRIPTION("NXP S32 common chassis GMAC driver");
 MODULE_LICENSE("GPL v2");
 
